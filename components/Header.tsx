0d0195682640227145efc7d145d837ecb28fd032
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-09-22 19:53:22 +0100

    updated

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="fixed top-0 left-0 w-full bg-background/70 text-white py-3 px-6 shadow-md border-b flex justify-between items-center backdrop-blur-md z-50">
+      <h1 className="text-xl font-semibold">Price Oracle</h1>
+      <p className="text-sm">Login / Register</p>
+    </header>
+  );
+};
+
+export default Header;
